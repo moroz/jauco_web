b0b9b60ef356866db760be10267aa92715b23806
Karol M <dmuhafc@gmail.com>
2018-04-08 23:41:32 +0200

    Modularize stylesheets, convert header.php to jade

--- index.php
+++ index.php
@@ -9,4 +9,5 @@
 if (!defined('ABSPATH')) {
     exit('restricted access');
 }
- ?><?php echo get_header(); ?><div class="primary-content"><header><h1><?php echo get_template_part('inc/templates/title'); ?></h1><?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); ?><article><?php echo get_template_part('content', get_post_format()); ?></article><?php endwhile; ?><?php echo brunch_paging_nav(); ?><?php else : ?><?php echo get_template_part('content', 'none'); ?><?php endif; ?></header></div><?php echo get_sidebar(); ?><?php echo get_footer(); ?>
+ ?><?php echo get_header(); ?><div class="homepage-banner"><div class="homepage-banner-text"><h1>Your new favorite tapas bar.</h1><h2>In the heart of Barcelona.</h2></div></div><div class="primary-content"><header><h1><?php echo get_template_part('inc/templates/title'); ?></h1><?php if (have_posts()) :
+  while (have_posts()) : the_post(); ?><article><?php echo get_template_part('content', get_post_format()); ?></article><?php endwhile; ?><?php echo brunch_paging_nav(); ?><?php else : ?><?php echo get_template_part('content', 'none'); ?><?php endif; ?></header></div><?php echo get_sidebar(); ?><?php echo get_footer(); ?>
