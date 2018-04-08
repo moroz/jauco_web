5f68533353c44923d3c08f4e8f0a7f619474bb9e
Karol M <dmuhafc@gmail.com>
2018-04-08 14:15:00 +0200

    Setup workflow for assets

--- functions.php
+++ functions.php
@@ -28,3 +28,12 @@ foreach ($brunch_includes as $file) {
     require_once $filepath;
 }
 unset($file, $filepath);
+
+// remove wp version param from any enqueued scripts
+function vc_remove_wp_ver_css_js( $src ) {
+    if ( strpos( $src, 'ver=' ) )
+        $src = remove_query_arg( 'ver', $src );
+    return $src;
+}
+add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
